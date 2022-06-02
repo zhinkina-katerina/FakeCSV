70242ee4db6f58cf5438ce0c077e8bbe6fadc4f4
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-02 11:06:13 +0300

    feat(model): add DataType model - add fixture datatypes.json

--- schemas/models.py
+++ schemas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class DataType(models.Model):
+    title = models.CharField(max_length=120)
+    data_type = models.CharField(max_length=120)
+    minimum = models.IntegerField(blank=True)
+    maximum = models.IntegerField(blank=True)
