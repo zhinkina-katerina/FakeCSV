3f018777130c6251a6b52345d13f08cc16a92c2e
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-02 11:14:39 +0300

    feat(model): add Schema model

--- schemas/models.py
+++ schemas/models.py
@@ -6,3 +6,10 @@ class DataType(models.Model):
     data_type = models.CharField(max_length=120)
     minimum = models.IntegerField(blank=True)
     maximum = models.IntegerField(blank=True)
+
+class Schema(models.Model):
+    title = models.CharField(max_length=100)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    structure = models.TextField()
