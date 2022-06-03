eda47f772f5631d0253b329f67101a2fe9424a76
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-03 18:46:04 +0300

    feat(view): add scheme edit page - add SchemeEditView - add SchemaForm and DataTypeForm - add DataTypeManager - add url for SchemeEditView - add edit_schema.html - add form_managment_edit_schema.js

--- schemas/urls.py
+++ schemas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.SchemeList.as_view(), name='schema_view'),
-    path('edit_schema/', views.BirdAddView.as_view(), name='schema'),
+    path('edit_schema/', views.SchemeEditView.as_view(), name='schema'),
 ]
