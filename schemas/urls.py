eb40869cd2eb1d8eb53e579ffe281c0cb1d3ff7d
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-05 22:17:18 +0300

    feat(url):add paths to schemas url

--- schemas/urls.py
+++ schemas/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.SchemeList.as_view(), name='schema_view'),
-    path('edit_schema/', views.SchemeEditView.as_view(), name='schema'),
+    path('<int:id>/dataset', views.DatasetView.as_view(), name="dataset_list"),
+    path('create_schema/', views.SchemeCreateView.as_view(), name='schema_create'),
+    path('delete_schema/<int:id>/', views.DeleteSchemaView.as_view(), name='schema_delete'),
+    path('edit_schema/<int:id>/', views.SchemeEditView.as_view(), name='schema_edit')
 ]
