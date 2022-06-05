ed122d9471c07afe88e3e1dcbdd7095a14d40cd5
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-05 21:53:52 +0300

    fix(model): fix models - add rows_quantity to Dataset - add order to DataType - add csv_file instead url in Dataset - add user to Dataset and Schema

--- schemas/models.py
+++ schemas/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 import json
+from django.contrib.auth.models import User
+
 
 
 class DataTypeManager(models.Manager):
     def get_titles(self):
         queryset = self.get_queryset()
-        result = [(x.id, x.title) for x in queryset]
+        result = [(x.title, x.title) for x in queryset]
         return result
 
     def get_items_have_range(self):
         queryset = self.get_queryset()
-        result = {x.id: x.has_editable_range for x in queryset}
+        result = {x.title: x.has_editable_range for x in queryset}
         jsonString = json.dumps(result, indent=4)
         return jsonString
 
@@ -21,6 +23,7 @@ class DataType(models.Model):
     minimum = models.IntegerField(blank=True)
     maximum = models.IntegerField(blank=True)
     has_editable_range = models.BooleanField(default=False)
+    order = models.IntegerField(blank=True)
 
     objects = DataTypeManager()
 
@@ -38,12 +41,14 @@ class Schema(models.Model):
     )
 
     title = models.CharField(max_length=100)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     structure = models.TextField()
     column_separator = models.CharField(max_length=10, default=',', choices=COLUMN_SEPARATOR_CHOICES)
     string_character = models.CharField(max_length=10, default='"', choices=STRING_CHARACTER_CHOICES)
+
+
 
 class Dataset(models.Model):
     STRING_STATUS_CHOICES = (
@@ -52,8 +57,10 @@ class Dataset(models.Model):
         ('Completed', 'Completed'),
         ('Failed', 'Failed'),
     )
-    url = models.URLField()
+    csv_file = models.FileField(upload_to='datasets/', null=True, blank=True)
     status = models.CharField(max_length=20, default='New', choices=STRING_STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     schema = models.ForeignKey(Schema, on_delete=models.CASCADE)
+    rows_quantity = models.IntegerField(default=0)
+
